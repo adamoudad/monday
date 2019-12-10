1dfd9f7da5e0ff078471b235f74b631d8655c53d
adamoudad <adam.oudad@gmail.com>
2019-12-11 02:39:10 +0900

    Add sentiment analysis example using nltk

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,14 +1,33 @@
-import sys
+# import nltk
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# from nltk.tokenize import word_tokenize
+# from nltk.corpus import stopwords
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# nltk.download('vader_lexicon')
+
+
+import nltk
+import random
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from nltk.classify import ClassifierI
+from statistics import mode
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+import re
 
-nltk.download('vader_lexicon')
 
 if __name__ == "__main__":
-    text = """Today is a great day."""
-    sia = SentimentIntensityAnalyzer()
-    print("Sentiment Score:", sia.polarity_scores(passage)['compound'])
+    positive_text = """Today is a great day. I love my life. Everything works fine. It is so awesome. We are the world!"""
+    negative_text = """I failed everything. Even my success are failure. I have nowhere to go. Everything is dark in my head. I hate you all."""
+    
+    # sia = SentimentIntensityAnalyzer()
+    # print("Posistive text:", positive_text)
+    # print("Sentiment Score:", sia.polarity_scores(positive_text)['compound'])
+    # print("Negative text:", negative_text)
+    # print("Sentiment Score:", sia.polarity_scores(negative_text)['compound'])
+    
 
     
